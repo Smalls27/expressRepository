fb052f8be6248d5023b788070e9c8e562a5f617b
Kenneth Smalls <kennethsmalls244@gmail.com>
2021-05-15 13:24:29 -0400

    Week 1 Assignment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnersRouter = require('./routes/partnersRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,6 +11,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnersRouter);
 
 app.use(express.static(__dirname + '/public'));
 
